87f92e1789a8f21002e8dbe4c0c56b11ad70a773
Justin Leniger <jleniger@mines.edu>
2014-02-19 13:31:55 -0700

    Added IntBoard JUnit test stubs

---
+++ src/clueTests/IntBoardTests.java
@@ -0,0 +1,20 @@
+package clueTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class IntBoardTests {
+	
+	@Before
+	public void before() {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
