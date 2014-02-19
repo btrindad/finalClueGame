9076a1a37520a8371f7a6b410d72eff6e93cd5d7
Justin Leniger <jleniger@mines.edu>
2014-02-19 13:19:40 -0700

    First Class
    Method stubs added for IntBoard class. No functionality or instance
    variables yet.

---
+++ src/clue/IntBoard.java
@@ -0,0 +1,33 @@
+package clue;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntBoard {
+	
+	public IntBoard() {
+		
+	}
+	
+	public void calcAdjacencies() {
+		
+	}
+	
+	public void startTargets() {
+		
+	}
+	
+	public Set<Integer> getTargets() {
+		return new HashSet<Integer>();
+	}
+	
+	public ArrayList<Integer> getAdjList() {
+		return new ArrayList<Integer>();
+	}
+	
+	public int calcIndex(int row, int columnn) {
+		return -1;
+	}
+
+}
