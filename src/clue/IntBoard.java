576d28531709583c77e9da2f6a4842bdc22b48f3
Justin Leniger <jleniger@mines.edu>
2014-02-21 10:29:51 -0700

    Began implementation of some methods.
    calcIndex method now works correctly and getAdjList is very close.

--- src/clue/IntBoard.java
+++ src/clue/IntBoard.java
@@ -1,15 +1,44 @@
 package clue;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class IntBoard {
+	private static final int BOARD_ROWS = 4;
+	private static final int BOARD_COLUMNS = 4;
+	
+	private Map<Integer, ArrayList<Integer>> adjMtx;
 	
 	public IntBoard() {
 		
 	}
 	
 	public void calcAdjacencies() {
+		adjMtx = new HashMap<Integer, ArrayList<Integer>>();
+		for (int row = 0; row < 4; row++) {
+			for (int column = 0; column < 4; column++) {
+				int index = calcIndex(row, column);
+				adjMtx.put(index, new ArrayList<Integer>());
+				// ABOVE
+				if (row > 0) {
+					adjMtx.get(index).add(calcIndex(row - 1, column));
+				}
+				// BELOW
+				if (row < (BOARD_ROWS - 1)) {
+					adjMtx.get(index).add(calcIndex(row + 1, column));
+				}
+				// LEFT
+				if (column > 0) {
+					adjMtx.get(index).add(calcIndex(row, column - 1));
+				}
+				// RIGHT
+				if (row < (BOARD_COLUMNS - 1)) {
+					adjMtx.get(index).add(calcIndex(row, column + 1));
+				}
+			}
+		}
 		
 	}
 	
@@ -22,11 +51,11 @@ public class IntBoard {
 	}
 	
 	public ArrayList<Integer> getAdjList(int location) {
-		return new ArrayList<Integer>();
+		return adjMtx.get(location);
 	}
 	
-	public int calcIndex(int row, int columnn) {
-		return -1;
+	public int calcIndex(int row, int column) {
+		return (4 * row) + column;
 	}
 
 }
