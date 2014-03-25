93edd718a4cebe486047bf91a3f6b9216f556872
Bernardo <btrindad@mymail.mines.edu>
2014-03-25 12:23:23 -0600

    colored in the board

--- src/clueBoard/WalkwayCell.java
+++ src/clueBoard/WalkwayCell.java
@@ -1,8 +1,10 @@
 package clueBoard;
 
+import java.awt.Color;
 import java.awt.Graphics;
 
 public class WalkwayCell extends BoardCell {
+	private final int borderLineWidth = 1;
 	
 	public WalkwayCell(int row, int column) {
 		super(row, column);
@@ -14,6 +16,10 @@ public class WalkwayCell extends BoardCell {
 	}
 	
 	public void draw(Graphics g, Board b) {
+		g.setColor(Color.black);
 		g.drawRect(getColumn()*(boardWidth/23), getRow()*(boardHeight/22), boardWidth/23, boardHeight/22);
+		g.setColor(Color.yellow);
+		g.fillRect(getColumn()*(boardWidth/23)+1, getRow()*(boardHeight/22)+1, 
+				(boardWidth/23)-borderLineWidth, (boardHeight/22)-borderLineWidth);
 	}
 }
