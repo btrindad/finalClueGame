064b24e223918bf5ade0abc8e0d20f9e7e96ddc7
rporter <rporter@mymail.mines.edu>
2014-03-24 21:00:04 -0600

    Started control GUI.

---
+++ src/clueGame/ControlGUI.java
@@ -0,0 +1,15 @@
+package clueGame;
+
+import javax.swing.JFrame;
+
+public class ControlGUI {
+
+	public static void main(String[] args) {
+		JFrame controlPanel = new JFrame();
+		controlPanel.setVisible(true);
+		controlPanel.setSize(800, 250);
+		controlPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
