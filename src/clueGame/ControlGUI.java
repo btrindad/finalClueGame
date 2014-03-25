a97562183817fb82cb963276684fb2f6f76e9459
rporter <rporter@mymail.mines.edu>
2014-03-24 21:53:58 -0600

    Not much progress...

--- src/clueGame/ControlGUI.java
+++ src/clueGame/ControlGUI.java
@@ -1,15 +1,23 @@
 package clueGame;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class ControlGUI {
 
 	public static void main(String[] args) {
-		JFrame controlPanel = new JFrame();
-		controlPanel.setVisible(true);
-		controlPanel.setSize(800, 250);
-		controlPanel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		JFrame controlGUI = new JFrame();
+		controlGUI.setVisible(true);
+		controlGUI.setSize(800, 250);
+		controlGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JButton nextPlayer = new JButton("Next player");
+		JButton makeAccusation = new JButton("Make an accusation");
+		controlGUI.add(nextPlayer, BorderLayout.NORTH);
+		controlGUI.add(makeAccusation, BorderLayout.EAST);
+		
+		
 	}
 
 }
